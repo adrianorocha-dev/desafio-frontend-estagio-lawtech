5ce66d89628322c64dd23019cf1037afd40566e5
Adriano Rocha <adriano.olr@gmail.com>
2020-08-09 12:45:21 -0300

    [Server] - Configura .env

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,10 @@ import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
 import { createConnection } from 'typeorm';
-
-import routes from './routes';
-
 import dotenv from 'dotenv';
 import path from 'path';
+
+import routes from './routes';
 
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
