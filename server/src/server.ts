b28f6e2643d2ececec1aef47c580416d0dcd33e3
Adriano Rocha <adriano.olr@gmail.com>
2020-08-09 12:44:48 -0300

    [Server] - Adiciona CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import { createConnection } from 'typeorm';
 
 import routes from './routes';
@@ -12,6 +13,7 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
